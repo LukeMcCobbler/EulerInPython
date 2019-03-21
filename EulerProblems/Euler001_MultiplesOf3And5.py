9d9b6bd17a6ca0e56efb69bfdcf6a13ea181c458
bolucpap <bolucpap@gmail.com>
2019-03-21 23:58:47 +0300

    Initial Commit

---
+++ EulerProblems/Euler001_MultiplesOf3And5.py
@@ -0,0 +1,12 @@
+import math
+def multiplesOfXUnderN(x,n):
+    multCount=math.floor((n-1)/x)
+    multSum=((multCount+1)*multCount)/2*x
+    return multSum
+def solveEuler001():
+    multsOf3Under100=multiplesOfXUnderN(3,1000)
+    multsOf5Under100=multiplesOfXUnderN(5,1000)
+    multsOf15Under100=multiplesOfXUnderN(15,1000)
+    retval=multsOf3Under100+multsOf5Under100-multsOf15Under100
+    return retval
+print(solveEuler001())
